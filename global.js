23384998675a220a9fcdf4fed2e4a6662cb5a042
bluehollowdev <99303285+hudsongoodman@users.noreply.github.com>
2022-07-19 13:55:52 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,11 @@
+
+function openmobilemenu(){
+document.getElementById("mobilemenu").style.display = "flex";
+document.getElementById("mobilemenucloseb").style.display = "block";
+document.getElementById("mobilemenub").style.display = "none";
+}
+function closemobilemenu(){
+document.getElementById("mobilemenu").style.display = "none";
+document.getElementById("mobilemenucloseb").style.display = "none";
+document.getElementById("mobilemenub").style.display = "block";
+}
