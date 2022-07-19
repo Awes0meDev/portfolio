a9c8dcad7d5f3253e641c356449ed0752679a83a
bluehollowdev <99303285+hudsongoodman@users.noreply.github.com>
2022-07-19 15:07:22 -0400

    Update global.js

--- global.js
+++ global.js
@@ -3,6 +3,8 @@ function openmobilemenu(){
 document.getElementById("mobilemenu").style.display = "flex";
 document.getElementById("mobilemenucloseb").style.display = "block";
 document.getElementById("mobilemenub").style.display = "none";
+document.body.style.overflowY = "hidden";
+
 }
 function closemobilemenu(){
 document.getElementById("mobilemenu").style.display = "none";
